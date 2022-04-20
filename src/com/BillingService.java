7d2c9e5484d9127a7bdc87095792c385dcc8e1d9
Amanthajay06 <amantha.jayasooriya99@gmail.com>
2022-04-21 02:54:32 +0530

    create AddBill service

---
+++ src/com/BillingService.java
@@ -0,0 +1,37 @@
+package com;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.FormParam;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import model.Billing;
+
+
+
+@Path("/Bill")
+public class BillingService {
+	
+Billing billobj = new Billing();
+	
+
+	@POST
+	@Path("/AddBill")
+	@Consumes(MediaType.APPLICATION_FORM_URLENCODED) 
+	@Produces(MediaType.TEXT_PLAIN)
+	public String insertUnitCount(@FormParam("AccountNumber") String AccountNumber, 
+								  @FormParam("name") String name, 
+								  @FormParam("unitCount") String unitCount, 
+								  @FormParam("month") String month,
+								  @FormParam("bamount") String bamount )
+								  {
+								 
+		
+		String output = billobj.insertUnitCount(AccountNumber, name, unitCount, month,bamount); 
+		
+		return output; 
+	}
+
+}
--- src/model/Billing.java
+++ src/model/Billing.java
@@ -31,6 +31,77 @@ public class Billing {
 		//Add unit count 
 			
 	    //Add unit count to the user's account
+	public String insertUnitCount(String accno, String uname, String unit, String bmonth, String bamount  ) { 
+				
+				String output = ""; 
+				
+				try { 
+					Connection con = connect(); 
+					
+					if (con == null) {
+						return "Error while connecting to the database for inserting."; } 
+						
+						// create a prepared statement
+						String query;
+					
+						query = " insert into billing_tb(`billID`,`AccountNumber`,`name`,`unitCount`,`month`,`billAmount`)" + " values (?, ?, ?, ?, ?,?)" ; 
+						PreparedStatement preparedStmt = con.prepareStatement(query);
+						 
+						// binding values
+						preparedStmt.setInt(1, 0); 
+						preparedStmt.setInt(2, Integer.parseInt(accno)); 
+						preparedStmt.setString(3, uname); 
+						preparedStmt.setFloat(4, Float.parseFloat(unit)); 
+						preparedStmt.setString(5, bmonth); 
+						
+						float no = Float.valueOf(unit.toString());
+						String billAmount= String.valueOf(calculateBill(no));
+						
+						preparedStmt.setFloat(6, Float.parseFloat(billAmount)); 
+						
+					
+						// execute the statement
+						preparedStmt.execute(); 
+						
+						con.close(); 
+						output = "Billing Details Inserted successfully"; 
+				} catch (Exception e) { 
+					output = "Error while inserting the details."; 
+					System.err.println(e.getMessage()); 
+				}
+				return output; 
+			}  
+
+			
+
+		//Calculate bill amount according to usage of unit
+		private float calculateBill(float no) {
+			
+		float sum=0;
+			 if (no <= 54) {
+				 return  sum=(float) (no*7.85);
+			 }
+			 if (54 < no && no <= 81) {
+				 
+				 return sum= (float) ((54 * 7.85) + ((no - 54) * 10)+ 90);
+			 }
+			 if (81 < no && no <= 108) {
+				 
+				 return sum= (float) ((54 * 7.85) + (27 * 10)+ ((no - 81)*27.75) + 480);
+			 }
+			 
+			 if (108 < no && no <= 162) {
+				 
+				 return sum= (float) ((54 * 7.85) + (27 * 10)+ (27 * 27.75) + ((no - 108)*32) + 480);
+			 }
+			 
+			 if (no >162 )
+			     return sum =  (float) ((54 * 7.85) + (27 * 10)+ (27 * 27.75) +  (54*32) + ((no - 162)*45) +540 );
+			 
+			return sum;
+			
+			
+		}
 
 		
 		
