f33dd62e8ad87cfb1d40d6942c6a51f83744c283
Gunarathne H. M. Y. B. it20237486 <it20237486@my.sliit.lk>
2022-04-24 05:21:38 +0530

    Created Payment Delete Method

--- src/model/Payment.java
+++ src/model/Payment.java
@@ -207,5 +207,37 @@ public class Payment {
 							 return output; 
 							 }
 				
+				// Delete buyer in the table
+				public String deletePayment(String PaymentID) {
+					String output = "";
+
+					try {
+						Connection con = connect();
+
+						if (con == null) {
+							return "Error while connecting to the database for deleting.";
+						}
+
+						// create a prepared statement
+						String query = "delete from payment where PaymentID=?";
+						PreparedStatement preparedStmt = con.prepareStatement(query);
+
+						// binding values
+						preparedStmt.setInt(1, Integer.parseInt(PaymentID));
+
+						// execute the statement
+						preparedStmt.execute();
+						con.close();
+
+						output = "payment details Deleted successfully";
+
+					} catch (Exception e) {
+						output = "Error while deleting the payment details.";
+						System.err.println(e.getMessage());
+					}
+
+					return output;
+				}
+				
 
 }
